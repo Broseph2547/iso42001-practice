c5d800eba6a939eb55ab3de7934650cc8fdfe9dc
Broseph2547 <cozzupj@gmail.com>
2026-07-15 14:42:50 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,51 @@
-const CACHE="iso42001-expert-v1";
-const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="iso42001-complete-v9-iphone-navigation";
+const FILES=[
+  "./",
+  "./index.html",
+  "./core.html",
+  "./scenarios.html",
+  "./expert.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
+      self.clients.claim()
+    ])
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.mode==="navigate"){
+    event.respondWith(
+      fetch(event.request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html")))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      const network=fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      }).catch(()=>cached);
+      return cached||network;
+    })
+  );
+});
